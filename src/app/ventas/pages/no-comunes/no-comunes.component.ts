b7bf06abacbe6333afc709aeb993a52b45c09aa5
Elanny Torres <torres.elanny@gmail.com>
2022-05-03 10:03:27 -0500

    data needed and fuctionality for i18nSelect and i18nPlural Pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,11 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  //* i18nSelect
+  nombre: string = 'Harold';
+  genero: string = 'masculino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
 
+  cambiarPersona() {
+    this.nombre = 'Elanny';
+    this.genero = 'femenino';
+  }
+
+  //* i18nPlural
+  clientes = ['Maria', 'Pedro', 'Juan', 'Fernando', 'Eduardo'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  borrarCliente() {
+    this.clientes.shift()
+  }
 }

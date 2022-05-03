255050e9aa2642307672077a5042b6b37318ce13
Elanny Torres <torres.elanny@gmail.com>
2022-05-02 20:24:17 -0500

    adding the animation functionality of PrimeNg and fieldset

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 //* PrimeNg
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 
 
@@ -12,7 +13,8 @@ import { MenubarModule } from 'primeng/menubar';
     CommonModule,
     ButtonModule,
     CardModule,
-    MenubarModule
+    MenubarModule,
+    FieldsetModule
   ]
 })
 export class PrimeNgModule { }

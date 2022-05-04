156b48e40fb66a2b6d428458238b83879190ff49
Elanny Torres <torres.elanny@gmail.com>
2022-05-03 23:33:47 -0500

    custome pipes, PrimeNG table, and its animations

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,15 +6,18 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     CommonModule,
     ButtonModule,
     CardModule,
+    FieldsetModule,
     MenubarModule,
-    FieldsetModule
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MayusculasPipe } from '../../pipes/mayusculas.pipe';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,17 +7,48 @@ import { MayusculasPipe } from '../../pipes/mayusculas.pipe';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
   enMayusculas: boolean = true;
 
-  constructor() { }
+  ordenarPor: string = '';
 
-  ngOnInit(): void {
-  }
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ];
 
   cambiar() {
     this.enMayusculas = !this.enMayusculas;
   }
+
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
+    console.log(valor);
+  }
+
 
 }
